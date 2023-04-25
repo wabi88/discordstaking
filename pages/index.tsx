a6cd1209723600386f6b6789c816ac6cb548ffd3
wabi88 <126939524+wabi88@users.noreply.github.com>
2023-04-24 18:46:18 -0700

    DiscordMember
    Discord Member staking production build

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,15 +13,15 @@ const Home: NextPage = () => {
 
 <h3><em><b>Welcome Discord Members!</b></em></h3>
     
-      <h5>As a token of appreciation to our growing community we've implemented a Discord Staking app <br/>
+      <h5>As a token of appreciation to our growing community weve implemented a Discord Staking app <br/>
       The way it works, members can request a Discord Member NFT Airdrop on our srd-pump Discord Channel. <br/><br/>
-      Occasionally, we'll Deposit Rewards(SRD) into the app and Discord Members will then be able to <br/>
+      Occasionally, well Deposit Rewards(SRD) into the app and Discord Members will then be able to <br/>
       connect their wallets to the app and start staking SRD! Members will be notified through our Discord <br/>
       srd-pump channel when staking is enabled and disabled.<br/><br/>
       SRD is the official currency in our Crypto Voyager Play-To-Earn Blockchain game,<br/>
       it can be used to purchase Premium Planets with extra staking power.<br/>
       SRD also has a liquidity pool started on UniSwap, making SRD tradeable for other ERC20 tokens!
-      <em>*Note: All Discord Member NFTs are set to "unstake" once staking has been disabled*</em></h5>
+      <em>*Note: All Discord Member NFTs are set to unstake once staking has been disabled*</em></h5>
       
       <a href="https://discord.gg/JT7tv2Kypv"><button type="button">Join Discord!</button></a>
       <a href="https://thecryptovoyager.com/"><button type="button">Crypto Voyager</button></a>
